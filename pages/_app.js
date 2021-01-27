d86374d755f9d2fb4aedd8685e822f5d2eccad5e
gimenezes <menezes.s.gilda@gmail.com>
2021-01-27 12:39:49 -0300

    change name quiz, layout, add images

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,52 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import Head from 'next/head';
+import db from "../db.json";
 
 const GlobalStyle = createGlobalStyle`
+
+* {
+  box-sizing: border-box;
+}
+
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+   display: flex;
+   flex-direction: column;
+    font-family: 'Capriola', sans-serif;
+    color: ${({ theme }) => theme.colors.contrastText};
   }
-`
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+  html, body {
+    min-height: 100vh;
+  }
+
+  #__next{
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Capriola&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" href="./images/icon.png" />
+        <title>The Vikings Quiz | Imersão Alura</title>
+
+      </Head>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
---
+++ src/components/Widget/index.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+const Widget = styled.div`
+  margin: 24px 0;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  background-color: #33658a;
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1,
+  h2,
+  h3 {
+    font-size: 1rem;
+    line-height: 1rem;
+    margin-bottom: 0;
+  }
+
+  p {
+    font-size: 0.875rem;
+    line-height: 1rem;
+  }
+`;
+
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+Widget.Header = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.text2};
+`;
+
+export default Widget;
